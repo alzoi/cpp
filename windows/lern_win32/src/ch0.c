3bfb09f21d97c39624847867530b47fe46b67968
alzoi <alzoi@ya.ru>
2023-01-31 17:45:14 +0500

    Update ch0.c

--- windows/lern_win32/src/ch0.c
+++ windows/lern_win32/src/ch0.c
@@ -14,6 +14,9 @@
 #include <stdio.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HFONT FAR PASCAL MyCreateFont(void);
+
+HFONT g_font;
 
 int WINAPI wWinMain
 (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
@@ -25,6 +28,8 @@ int WINAPI wWinMain
     _snwprintf_s(cmd_buffer, 1024, 1023, L"%s", lpCmdLine);
     MessageBoxW(NULL, cmd_buffer, L"Параметры командной строки", MB_ICONINFORMATION);
   }
+
+  g_font = MyCreateFont();
 
   // Регистрируем класс окна.
   const wchar_t CLASS_NAME[]  = L"Win_Class_hello";
@@ -101,10 +106,13 @@ LRESULT CALLBACK WindowProc
         
         HFONT hFont, hOldFont; 
 
+        // Получаем системный шрифт.
         hFont = (HFONT)GetStockObject(ANSI_VAR_FONT); 
+        // Устанавливаем логический шрифт.
+        hFont = g_font;
 
         if (hOldFont = (HFONT)SelectObject(hdc, hFont)) {          
-          DrawTextExW(hdc, str, wsprintf(str, L"Изучение Win API\nУрок номер %d", 1), &rect2, flags, NULL);
+          DrawTextExW(hdc, str, wsprintf(str, L"Изучение Win32 API\nУрок номер %d", 1), &rect2, flags, NULL);
           SelectObject(hdc, hOldFont); 
         }
 
@@ -122,4 +130,34 @@ LRESULT CALLBACK WindowProc
   }
   // Передаём сообщение обработчику по умолчанию.
   return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+HFONT FAR PASCAL MyCreateFont(void) {
+// https://learn.microsoft.com/ru-ru/windows/win32/gdi/creating-a-logical-font
+
+  CHOOSEFONT cf; 
+  LOGFONT lf; 
+  HFONT hfont; 
+
+  cf.lStructSize    = sizeof(CHOOSEFONT); 
+  cf.hwndOwner      = (HWND)NULL; 
+  cf.hDC            = (HDC)NULL; 
+  cf.lpLogFont      = &lf; 
+  cf.iPointSize     = 0; 
+  cf.Flags          = CF_SCREENFONTS; 
+  cf.rgbColors      = RGB(0, 0, 0); 
+  cf.lCustData      = 0L; 
+  cf.lpfnHook       = (LPCFHOOKPROC)NULL; 
+  cf.lpTemplateName = (LPCWSTR)NULL; 
+  cf.hInstance      = (HINSTANCE)NULL; 
+  cf.lpszStyle      = (LPWSTR)NULL; 
+  cf.nFontType      = SCREEN_FONTTYPE; 
+  cf.nSizeMin       = 0; 
+  cf.nSizeMax       = 0; 
+
+  ChooseFont(&cf); 
+
+  // Создаём логический шрифт.
+  hfont = CreateFontIndirect(cf.lpLogFont); 
+  return (hfont); 
 }
