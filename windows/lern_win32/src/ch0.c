5d604ef021145a662d4bb32102487e7656953e24
alzoi <alzoi@ya.ru>
2023-01-31 15:34:07 +0500

    Update ch0.c

--- windows/lern_win32/src/ch0.c
+++ windows/lern_win32/src/ch0.c
@@ -1,3 +1,4 @@
+// Привет, Мир! Unicode версия на Win API.
 // Файл ch0.c
 // gcc ch0.c -o ch0.exe -s -lkernel32 -luser32 -lgdi32 -mwindows -municode
 
@@ -18,31 +19,33 @@ int WINAPI wWinMain
 (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
   
   // В модальном окне выводим параметры командной строки.
-  if (*lpCmdLine) {
+  if (*lpCmdLine != 0) {
     wchar_t cmd_buffer[1024];
-    swprintf_s(cmd_buffer, 1024, L"%s", lpCmdLine);
-    MessageBoxW(NULL, cmd_buffer, L"Сообщение", 0);
+    //swprintf_s(cmd_buffer, 1024, L"%s", lpCmdLine);
+    _snwprintf_s(cmd_buffer, 1024, 1023, L"%s", lpCmdLine);
+    MessageBoxW(NULL, cmd_buffer, L"Параметры командной строки", MB_ICONINFORMATION);
   }
 
   // Регистрируем класс окна.
-  const wchar_t CLASS_NAME[]  = L"Sample Window Class";
+  const wchar_t CLASS_NAME[]  = L"Win_Class_hello";
   WNDCLASS wc = { };
 
   wc.lpfnWndProc   = WindowProc;
+  wc.style         = CS_HREDRAW | CS_VREDRAW;
   wc.hInstance     = hInstance;
   wc.lpszClassName = CLASS_NAME;
+  wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
   wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+  wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); // Цвет заливки фона.
 
   RegisterClass(&wc);
 
   // Создаём окно.
-  HWND hwnd = CreateWindowEx(
-    0,
+  HWND hwnd = CreateWindowEx(0,
     CLASS_NAME,
-    L"Learn to Program Windows",
-    WS_OVERLAPPEDWINDOW,            // Стиль окна.
-    // Размер и положение окна.
-    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+    L"Окно Win API",
+    WS_OVERLAPPEDWINDOW, // Стиль окна.
+    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, // Размер и положение окна.
     NULL,
     NULL, // Меню отсутствует.
     hInstance,
@@ -54,7 +57,7 @@ int WINAPI wWinMain
   }
 
   ShowWindow(hwnd, nShowCmd);
-  UpdateWindow(hwnd);
+  //UpdateWindow(hwnd);
 
   // Цикл обработки сообщений ОС.
   MSG msg = { };
@@ -63,11 +66,11 @@ int WINAPI wWinMain
     DispatchMessage(&msg);
   }
 
-  return 0;
+  return msg.wParam;
 }
 
-LRESULT CALLBACK
-WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindowProc
+(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   
   switch (uMsg) {
     case WM_DESTROY:
@@ -76,14 +79,15 @@ WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
     case WM_PAINT: {
       PAINTSTRUCT ps;
+      RECT rect;
 
       HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+        //FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW+1));
+        GetClientRect(hwnd, &rect);
+        DrawText(hdc, L"Привет, мир!", -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
       EndPaint(hwnd, &ps);
-      
       return 0;
     }
-    return 0;
   }
   return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
