bab37acd6f235efdac5499a1770e9022db3b34f8
alzoi <alzoi@ya.ru>
2023-09-27 15:09:48 +0500

    Update ch01.cpp

--- windows/lern_win32/src/ch01.cpp
+++ windows/lern_win32/src/ch01.cpp
@@ -1,5 +1,5 @@
 // g++.exe -Wall -O2 -c ch01.cpp -o ch01.o
-// g++.exe -s -lgdi32 -luser32 -lkernel32 -lcomctl32 -luxtheme -mwindows ch01.o -o ch01.exe 
+// g++.exe ch01.o -o ch01.exe -s -lgdi32 -luser32 -lkernel32 -lcomctl32 -luxtheme -mwindows 
 
 #define UNICODE
 
